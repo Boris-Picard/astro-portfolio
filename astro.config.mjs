69c7560cd6cae39d9c8b86b6535b1d0cdd9c14fd
Boris-Picard <picard.boris@gmail.com>
2025-05-31 18:48:53 +0200

    fix: components path

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,16 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
 import sitemap from '@astrojs/sitemap';
+import path from "path";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
+    resolve: {
+      alias: {
+        "@": path.resolve("./src")
+      }
+    },
     plugins: [tailwindcss()]
   },
   site: "https://www.boris-picard.com/",
