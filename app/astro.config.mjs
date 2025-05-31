94bf69cb4afb93c66195cd41563c68ed6d2c8e39
Boris-Picard <picard.boris@gmail.com>
2025-05-31 10:24:27 +0200

    feat: seo

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -4,11 +4,13 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
-      plugins: [tailwindcss()]
-	},
-
-  integrations: [react()]
+    plugins: [tailwindcss()]
+  },
+  site: "https://www.boris-picard.com/",
+  integrations: [react(), sitemap()]
 });
